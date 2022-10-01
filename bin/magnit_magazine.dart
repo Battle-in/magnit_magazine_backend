f3c1bcd42d6ff923bf53ba31939764be1294dccb
Ivan <i.battle_in@kidsbe.ru>
2022-10-01 22:00:44 +0500

    first commit

---
+++ bin/magnit_magazine.dart
@@ -0,0 +1,5 @@
+import 'package:magnit_magazine/magnit_magazine.dart' as magnit_magazine;
+
+void main(List<String> arguments) {
+  print('Hello world: ${magnit_magazine.calculate()}!');
+}
---
+++ test/magnit_magazine_test.dart
@@ -0,0 +1,8 @@
+import 'package:magnit_magazine/magnit_magazine.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
